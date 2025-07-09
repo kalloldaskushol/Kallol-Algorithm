69993b5a750c752ce221ab3e942591b3beda9d73
Kallol Das Kushol <kallold70@gmail.com>
2025-07-10 00:19:48 +0600

    normal

--- Class/9_Fibonachi_Memoization.cpp
+++ Class/9_Fibonachi_Memoization.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 // stack size = n, bcz base call doesnt stores
 // we can't get 100 bcz of space.
 int FiboMemo(int n, int F[]){
@@ -9,18 +8,15 @@ int FiboMemo(int n, int F[]){
         F[n] = n;
         return n;
     }
-
     // if value -1 work
     if(F[n] == -1){
         F[n] = FiboMemo(n-1, F) + FiboMemo(n-2, F); // stack size is defined by the lowest value.
     }
-
     // if its not -1 and not base case return F[n]
     return F[n];
 }
 
 int main(){
-
     int n;
     cin >> n;
     int F[n+1]; // why
