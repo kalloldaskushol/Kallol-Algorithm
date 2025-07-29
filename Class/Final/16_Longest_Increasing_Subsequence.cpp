09c7b989bc74d858a1ccd4554d7dc6d72cf94800
Kallol Das Kushol <kallold70@gmail.com>
2025-07-29 14:52:08 +0600

    normal

--- Class/Final/16_Longest_Increasing_Subsequence.cpp
+++ Class/Final/16_Longest_Increasing_Subsequence.cpp
@@ -33,7 +33,17 @@ int main(){
         LIS[i] = 1;
     }
 
-    
+    for(int i = 1; i<n; i++){
+        for(int j = 0; j<i; j++){
+            if(list[j] < list[i]){
+                LIS[i] = max(LIS[i], 1+LIS[j]);
+            }
+        }
+    }
+
+    for(int i = 0; i<n; i++){
+        cout << LIS[i] << " ";
+    }
 
     return 0;
 }
