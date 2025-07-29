e16dc8390bac66dec403159b3e66aaa31eaf42bc
Kallol Das Kushol <kallold70@gmail.com>
2025-07-29 15:18:10 +0600

    normal

--- Class/Final/16_Longest_Increasing_Subsequence.cpp
+++ Class/Final/16_Longest_Increasing_Subsequence.cpp
@@ -1,3 +1,5 @@
+// ideone.com/8r1vQg
+
 /*
 memset(arr, 0, sizeof(arr)) its stored values by byte
 00000000-00000000-00000000
@@ -37,6 +39,10 @@ int main(){
         for(int j = 0; j<i; j++){
             if(list[j] < list[i]){
                 LIS[i] = max(LIS[i], 1+LIS[j]);
+                
+                if(LIS[i] == 1+LIS[j]){
+                    Tracker[i] = j;
+                }
             }
         }
     }
@@ -44,6 +50,35 @@ int main(){
     for(int i = 0; i<n; i++){
         cout << LIS[i] << " ";
     }
+    cout << endl;
+    
 
+    // not always at last
+
+    int mx = INT_MIN;
+    int maxidx;
+    for(int i = 0; i<n; i++){
+        if(mx < LIS[i]) {
+            mx=LIS[i];
+            maxidx = i;
+    }
+}
+    cout << "LIS Length = " << mx << endl;
+    for(int i = 0; i<n; i++){
+        cout << Tracker[i] << " ";
+    }
+    cout << endl;
+
+    // Sequence 
+    vector<int> ans;
+    for(int i = maxidx; i>=0; ){
+        ans.push_back(list[i]);
+        i = Tracker[i];
+    }
+
+    for(int i = ans.size()-1 ; i>= 0; i--){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
